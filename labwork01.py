ba909cbe7662be09bb49c880e080c43166aac366
Nguyễn Tất Hùng <hung.nguyen@bluecoglobal.com>
2021-12-05 11:50:59 +0700

    update caculator DF

--- labwork01.py
+++ labwork01.py
@@ -1,10 +1,11 @@
 import json
 
-infile = "yelp_academic_dataset_review.json"
+infile = "input_file.txt"
 outfile = "cleaned_file.json"
 
 delete_list1 = [" a ", " in ", " of ", " the ", " at ", " line ", " get "]
 delete_list2 = [";", ":", ".", ",", "?", "!", "(", ")", "}", "{", "\"", "\'"]
+dict_word = {};
 with open(infile) as fin, open(outfile, "w+") as fout:
     for line in fin:
         for word in delete_list1:
@@ -12,3 +13,13 @@ with open(infile) as fin, open(outfile, "w+") as fout:
         for word in delete_list2:
             line = line.replace(word, "")
         fout.write(line)
+        split_word = line.split();
+        for word in split_word:
+            if word in dict_word:
+                dict_word[word] = dict_word[word]+1
+            else:
+                dict_word[word] = 1
+
+for k, v in dict_word.items():
+    print(k, v)
+
