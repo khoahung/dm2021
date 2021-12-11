68e5ab87984e895c70a9a5a44a74d024809f6251
Nguyễn Tất Hùng <hung.nguyen@bluecoglobal.com>
2021-12-11 21:36:03 +0700

    labwork03

--- 03.review.length.clustering.py
+++ 03.review.length.clustering.py
@@ -4,14 +4,20 @@ cluter2 = [(5, 6), (7, 8), (5, 5),(3, 6),(7, 5)]
 distance = 0
 minimum = math.sqrt(pow((cluter1[0][0]-cluter2[0][0]), 2) + pow((cluter1[0][1]-cluter2[0][1]), 2))
 maximum = math.sqrt(pow((cluter1[0][0]-cluter2[0][0]), 2) + pow((cluter1[0][1]-cluter2[0][1]), 2))
+listDistance = []
 for i in cluter1:
     for j in cluter2:
         distance = math.sqrt(pow((i[0]-j[0]), 2) + pow((i[1]-j[1]), 2))
+        listDistance.append(round(distance,2))
         print(round(distance,2), end='   ')
         if maximum < distance:
             maximum = distance;
         if minimum > distance:
             minimum = distance;
     print('            ')
+
 print('max: {}'.format(round(maximum, 2)))
 print('min: {}'.format(round(minimum, 2)))
+
+listDistance.sort()
+print(listDistance)
